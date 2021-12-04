83c816d944feb1a6be29adc96d0f9f5dd847d230
Philipp Kastl <picastle@posteo.de>
2021-12-04 22:59:52 +0100

    setting up the get routes at index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
-// requires router function of express 
-const router = require('express').Router();
-
+// requires router function of express
+const router = require("express").Router();
 
 //usage of router for index
 router.get("/", (req, res, next) => {
-    res.render("index")
-})
+  res.render("index");
+});
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
+});
+router.get("/info", (req, res, next) => {
+  res.render("info");
+});
+router.get("/comments", (req, res, next) => {
+  res.render("comments");
+});
 
-module.exports = router
+module.exports = router;
